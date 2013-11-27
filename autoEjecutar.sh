5ad7a385bc8e5f9c64e21eb19462f1239bcb4d3b
Guillermo Julián <gjulian93@gmail.com>
2013-11-27 20:01:00 +0100

    Entrega

--- autoEjecutar.sh
+++ autoEjecutar.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 make clean && make
 echo "let's go with ejer 1" >> control.txt
-echo "#####################################3">> control.txt
+echo "#####################################">> control.txt
 ./scripts/genData
 echo "Done"
-echo "#####################################3">> control.txt
+echo "#####################################">> control.txt
+echo "Ejercicio 2" >> control.txt
 ./scripts/genDataCache
-echo "#####################################3">> control.txt
+echo "#####################################">> control.txt
 echo "Vamos con ejercicio 3">>control.txt
 ./scripts/genDataMatrix 
 #Una vez tenemos los datos generados vamos a por las gráficas
